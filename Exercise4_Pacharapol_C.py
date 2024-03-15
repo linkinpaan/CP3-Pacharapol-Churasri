0981259ce46fe6ece7489568ef59c5506d20a605
linkinpaan <163490435+linkinpaan@users.noreply.github.com>
2024-03-15 14:06:26 +0700

    Create Exercise4_Pacharapol_C.py

---
+++ Exercise4_Pacharapol_C.py
@@ -0,0 +1,10 @@
+fe = input()
+gb = input()
+ics = input()
+cp = input()
+
+print("---Your Score---")
+print("Foundation English: ",fe)
+print("General Business: ",gb)
+print("Introduction to Computer Systems: ",ics)
+print("Computer Programming: ",cp)
